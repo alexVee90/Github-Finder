4a2f9ef3914c4c4ebb1f9144343a8850b89e1a22
alex <alexandrucristianvisan@gmail.com>
2019-09-02 22:16:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/About';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null
+  };
+
+  searchUsers = async text => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_ID${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      users: res.data.items,
+      loading: false
+    });
+  };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  setAlert = (message, type) => {
+    this.setState({ 
+      alert: { message, type } 
+    });
+
+    setTimeout( () => { 
+      this.setState({ alert: null })
+    }, 3000)
+
+  };
+
+  render() {
+    const { users, loading } = this.state;
+    return (
+      <Router>     
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <div className='container'>
+          <Alert alert={this.state.alert} />
+          <Switch>
+            <Route 
+            exact path='/' 
+            render={ props => (
+              <Fragment>
+                <Search
+                  searchUsers={this.searchUsers}
+                  clearUsers={this.clearUsers}
+                  showClear={users.length > 0 ? true : false}
+                  setAlert={this.setAlert}
+                />
+                <Users loading={loading} users={users} />
+              </Fragment>
+            )}
+             />
+             <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </div>
+      </Router>   
+    );
+  }
 }
 
 export default App;
